060123c6ff3d2b84c2e9e4e41a55c36d13385400
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-07 13:19:45 +0000

    Create jquerytest.js

---
+++ jquery/scripts/jquerytest.js
@@ -0,0 +1,51 @@
+
+// best practice is to wrap the code around
+
+// $( document ).ready(function() {
+  // code goes in here
+// });
+
+//$( document ).ready(function() {
+    //$( "a" ).click(function( event ) {
+        //alert( "Thanks for visiting!" );
+    //});
+//});
+
+/* events:
+click	        keypress	submit	    load
+dblclick	    keydown	    change	    resize
+mouseenter	    keyup	    focus	    scroll
+mouseleave	 	blur	    unload */
+
+// example $("p").click();
+
+// mouseenter The function is executed when the mouse pointer enters the HTML element
+
+// place that describes all functions /www.w3schools.com/jquery/jquery_events.asp
+
+//  on event allows for more events
+
+$(document).ready(function(){
+    $("p").on({
+      mouseenter: function(){
+        $(this).css("background-color", "lightgray");
+      },  
+      mouseleave: function(){
+        $(this).css("background-color", "lightblue");
+      }, 
+      click: function(){
+        $(this).css("background-color", "yellow");
+      }  
+    });
+  });
+
+  /*
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").hide(1000);
+    });
+  }); */
+
+  $(".hide-show").click(function(){
+    $("p").toggle(1000);
+  });
